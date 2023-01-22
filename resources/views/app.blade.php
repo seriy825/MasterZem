aa09221e6bbe619d24865f944f15fb7278f1e4a1
Serhii Olar <olar_ss@groupbwt.com>
2023-01-22 13:46:58 +0200

    Initial commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>{{env('APP_NAME','MarketZem')}}</title>
+    <!-- Fonts -->
+    <link rel="icon" href="icon.png">
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+</head>
+<body>    
+    <div id="app">  
+        <router-view></router-view>
+    </div>
+     @vite('resources/js/app.js')
+</body>
+</html>
